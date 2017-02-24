bc75f53cfa1d739084ceabe9ee1ce2e26f19b242
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-24 11:29:37 -0800

    create Stylist construct method, test and method for getId

---
+++ src/Stylist.php
@@ -0,0 +1,19 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $id;
+
+        function __construct($new_name, $id = null)
+        {
+            $this->name = $new_name;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+    }
+?>
