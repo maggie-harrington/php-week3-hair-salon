771569d281828050a76f24fc5826550fa16b8995
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-24 16:23:30 -0800

    create method for update and pass test

--- src/Client.php
+++ src/Client.php
@@ -70,5 +70,11 @@
             return $found_client;
         }
 
+        function update($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE clients SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
     }
 ?>
