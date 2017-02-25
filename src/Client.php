7eb98b4078ee481eda87716758e8d6c5d7296c4f
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-24 16:15:54 -0800

    create method for find and pass test

--- src/Client.php
+++ src/Client.php
@@ -56,5 +56,19 @@
         {
             $GLOBALS['DB']->exec("DELETE FROM clients;");
         }
+
+        static function find($search_id)
+        {
+            $found_client = null;
+            $clients = Client::getAll();
+            foreach($clients as $client) {
+                $client_id = $client->getId();
+                if ($client_id == $search_id) {
+                  $found_client = $client;
+                }
+            }
+            return $found_client;
+        }
+
     }
 ?>
